2155227cc71a2bfe5b3cd307488c12deeb6c4973
kralljr <jenna.krall@gmail.com>
2021-08-26 14:26:15 -0400

    Fix documentation for data

---
+++ R/PM25.R
@@ -0,0 +1,21 @@
+#' PM2.5 monitoring data and FAQSD model predictions
+#' 
+#' A dataset of PM2.5 monitoring data and predictions from fused CMAQ for 17 monitors.  
+#' 
+#' \itemize{
+#' 		\item date. Date of observation
+#' 		\item monid. Monitor ID
+#' 		\item fips. FIPS code
+#' 		\item city. Major urban area where monitor is located
+#' 		\item longitude. Longitude
+#' 		\item latitude. Latitude
+#' 		\item FAQSD.  Predictions from the FAQSD model
+#' 		\item monitor. Monitor concentrations
+#'		...
+#'}
+#'
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 19888 rows and 8 variables
+#' @name PM25
+NULL
